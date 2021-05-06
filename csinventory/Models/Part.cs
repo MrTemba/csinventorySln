da360b163bec33452ab07647b6039aec2c96efae
Temba <temba61@gmail.com>
2021-05-06 15:34:46 +0200

    Defined the BD's connection string

---
+++ csinventory/Models/Part.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csinventory.Models
+{
+    public class Part
+    {
+        public long PartID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int PartNumber { get; set; }
+        [Column(TypeName="decimal(8,2)")]
+        public decimal UnitCost { get; set; }
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal TotalCost { get; set; }
+    }
+}
